5e81b0cccc6234554e2cad283b3b15f09cb6c1f8
Orenthal Hill <ojhill98@gmail.com>
2021-07-07 08:50:36 -0500

    add exteral.js internal.js complete exercise 3 prompt, alert,confirm

---
+++ js/external.js
@@ -0,0 +1,53 @@
+"use strict";
+
+// console.log("Hello from external Javascrpit");
+//
+// alert("Welcome to my website");
+// var userInput = prompt ('What is your Favorite color?');
+// console.log ('Great ' + userInput +(' is my favorite color too!'));
+
+// var rentalPerDayDollars = 3;
+// var littleMermaidDays = prompt("How many days would you like to rent movie?");
+// var brotherBearDays = prompt("How many days would you like to rent movie?");
+// var herculesDays = prompt("How many days would you like to rent movie?");
+// var totalRentalCost = (littleMermaidDays*rentalPerDayDollars)+(brotherBearDays*rentalPerDayDollars)+(herculesDays*rentalPerDayDollars);
+// var totalRentalCost = alert("Your total is $" + (parseInt(littleMermaidDays)+ parseInt(brotherBearDays)+ parseInt(herculesDays))* parseInt(rentalPerDayDollars));
+
+// var googleHourlyRateDollars = prompt("Google hourly rate?");
+// var amazonHourlyRateDollars = prompt("Amazon hourly rate?");
+// var facebookHourlyRateDollars = prompt("Facebook hourly rate?");
+// var googleHours = prompt ("Google hours worked?");
+// var amazonHours = prompt ("Amazon hours worked?");
+// var facebookHours = prompt ("Facebook hours worked?");
+// var totalPayment;
+// totalPayment = googleHours * googleHourlyRateDollars;
+// totalPayment += amazonHours * amazonHourlyRateDollars;
+// totalPayment += facebookHours * facebookHourlyRateDollars;
+// alert("Total Earned" + totalPayment);
+
+// var classIsNotFull; // boolean
+// var classSchedulesCheck; // boolean
+// var studentEnrolled = classIsNotFull && classSchedulesCheck;
+
+
+// var numberOfItemsForDiscount = 2;
+// var numberOfItems = prompt("how many items?");
+// var offerIsNotExpired = true;
+// var isPremiumMember = true;
+// var productDiscountApplied = isPremiumMember || (numberOfItems >= numberOfItemsForDiscount && offerIsNotExpired);
+// alert("You saved today!!!");
+
+// var username = 'codeup';
+// var password = 'notastrongpassword';
+//
+// var passIsCorrectLength = password.length >= 5;
+// var passIsTrim = password === password.trim();
+// var noUsernamePass = password.indexOf(username) === -1;
+// var usernameIsCorrectLength = username.length <= 20;
+// var usernameIsTrim = username === username.trim();
+// var passIsValid = passIsCorrectLength && noUsernamePass && passIsTrim;
+// var usernameIsValid = usernameIsCorrectLength && usernameIsTrim;
+// var isValidRegistration = passIsValid && usernameIsValid (alert("Registration is Valid!"));
+
+
+
