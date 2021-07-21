f1b62801659bfe42fe82d192575242d18ddaeba0
Orenthal Hill <ojhill98@gmail.com>
2021-07-20 19:48:10 -0500

    exercise 1,2,3 complete

---
+++ js/for_loops.js
@@ -0,0 +1,29 @@
+ //"use strict";
+// alert("this works");
+// function showMultiplicationTable(){
+//     var mutiplier = 7;
+//     for (var x=1; x<=10; x++){
+//        var result = mutiplier * x;
+//         console.log(mutiplier + " * " + x +" =" + result)
+//     }
+//
+// }
+// showMultiplicationTable();
+// function randomNumberGenerator(){
+//
+//     for (x=1;x<=10;x++){
+//         var random = Math.floor(Math.random()*180) +20;
+//             if (random % 2===0){
+//             console.log("The number"+ random +" is even");
+//             }else{
+//             console.log("The number" + random + " is odd");
+//         }
+//     }
+// }
+// randomNumberGenerator();
+// function nineSpaces(){
+//     for (i=1; i<=9; i++){
+//         console.log(i.toString().repeat(i));
+//     }
+// }
+// nineSpaces();
---
+++ js/while.js
@@ -0,0 +1,22 @@
+// var number = 1;
+// while (number < 65536){
+//     number *= 2;
+//     console.log(number);
+// }
+
+// var allCones = Math.floor(Math.random() * 50) + 50;
+//    console.log("Starting number" + allCones);
+//     do{
+//         var sold = Math.floor(Math.random() * 5) + 1;
+//         console.log("Customer purchased" + sold);
+//         if (allCones <sold){
+//             console.log("whoops we ran out");
+//
+//         }else{
+//             allCones = (allCones-sold);
+//             console.log(allCones);
+//
+//         }
+//
+//
+//     }while (allCones >0);
